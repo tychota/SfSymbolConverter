fdc0acb82dea7d6f21388f1bd62a99bc9d0370a2
laurence-liu-923 <laurence.0923@gmail.com>
2024-03-29 09:43:51 +0100

    feat: modify implementation for S scaled symbol

--- generate_sf_symbols.rb
+++ generate_sf_symbols.rb
@@ -3,11 +3,11 @@
 require "nokogiri"
 
 # Path to file exported from the SF Symbols app
-TEMPLATE_PATH = "assets/template/circle_static.svg"
+TEMPLATE_PATH = "assets/template/circle_variable.svg"
 # Path to one of the SVGs provided by the designers
-SOURCE_SVG_PATH = "assets/source_svg/user_online_test.svg"
+SOURCE_SVG_PATH = "assets/source_svg/add_test.svg"
 # Path to the SVG we are generating
-DESTINATION_SVG_PATH = "assets/destination_svg/user_online_test-symbol.svg"
+DESTINATION_SVG_PATH = "assets/destination_svg/add_test-symbol.svg"
 
 # Expected icon size
 ICON_WIDTH = 24
@@ -34,7 +34,9 @@ TEMPLATE_ICON_WEIGHTS = ["Black", "Heavy", "Bold", "Semibold", "Medium", "Regula
 TEMPLATE_ICON_SIZES.each do |size|
   TEMPLATE_ICON_WEIGHTS.each do |weight|
     id = "#{weight}-#{size}"
-    next if id == "Regular-M" # TODO: Only keep the S size
+    next if id == "Ultralight-S"  # TODO: Only keep the S size
+    next if id == "Regular-S"
+    next if id == "Black-S"
     if template_svg.at_css("##{id}") != nil then
       template_svg.at_css("##{id}").remove
     end
@@ -53,13 +55,13 @@ def get_guide_value(template_svg, axis, xml_id)
 end
 
 # Get the x1 (should be the same as x2) of the #left-margin node.
-original_left_margin = get_guide_value(template_svg, :x, "left-margin-Regular-M")
+original_left_margin = get_guide_value(template_svg, :x, "left-margin-Regular-S")
 # Get the x1 (should be the same as x2) of the #right-margin node.
-original_right_margin = get_guide_value(template_svg, :x, "right-margin-Regular-M")
+original_right_margin = get_guide_value(template_svg, :x, "right-margin-Regular-S")
 # Get the y1 (should be the same as y2) of the #Baseline-M node.
-baseline_y = get_guide_value(template_svg, :y, "Baseline-M")
+baseline_y = get_guide_value(template_svg, :y, "Baseline-S")
 # Get the y1 (should be the same as y2) of the #Capline-M node.
-capline_y = get_guide_value(template_svg, :y, "Capline-M")
+capline_y = get_guide_value(template_svg, :y, "Capline-S")
 
 # Load the SVG icon.
 icon_svg = File.open(SOURCE_SVG_PATH) do |f|
@@ -81,21 +83,21 @@ end
 # scale = ((baseline_y - capline_y).abs / ICON_HEIGHT) * ADDITIONAL_SCALING
 
 # We want the m scaled icon to be correlated with the font of size 14.
-scale_m = (baseline_y - capline_y).abs / 14
+scale_s = (baseline_y - capline_y).abs / 14 * 0.783
 horizontal_center = (original_left_margin + original_right_margin) / 2
 
-scaled_width = ICON_WIDTH * scale_m
-scaled_height = ICON_HEIGHT * scale_m
+scaled_width = ICON_WIDTH * scale_s
+scaled_height = ICON_HEIGHT * scale_s
 
 # If you use the template's margins as-is, the generated symbol's width will depend on the template chosen.
 # To not have to care about the template, we move the margin based on the computed symbol size.
 horizontal_margin_to_center = scaled_width / 2 + MARGIN_LINE_WIDTH + ADDITIONAL_HORIZONTAL_MARGIN
 adjusted_left_margin = horizontal_center - horizontal_margin_to_center
 adjusted_right_margin = horizontal_center + horizontal_margin_to_center
-left_margin_node = template_svg.at_css("#left-margin-Regular-M")
+left_margin_node = template_svg.at_css("#left-margin-Regular-S")
 left_margin_node["x1"] = adjusted_left_margin.to_s
 left_margin_node["x2"] = adjusted_left_margin.to_s
-right_margin_node = template_svg.at_css("#right-margin-Regular-M")
+right_margin_node = template_svg.at_css("#right-margin-Regular-S")
 right_margin_node["x1"] = adjusted_right_margin.to_s
 right_margin_node["x2"] = adjusted_right_margin.to_s
 
@@ -104,21 +106,27 @@ right_margin_node["x2"] = adjusted_right_margin.to_s
 symbol_svg = template_svg.dup
 
 # It's finally time to handle that important #Regular-M node.
-regular_m_node = symbol_svg.at_css("#Regular-M")
+regular_s_node = symbol_svg.at_css("#Regular-S")
 
 # Move the shape so its center is at the center of the guides.
 translation_x = horizontal_center - scaled_width / 2
 translation_y = (baseline_y + capline_y) / 2 - scaled_height / 2
 # Prepare a transformation matrix from the values calculated above.
 transform_matrix = [
-  scale_m, 0,
-  0, scale_m,
+  scale_s, 0,
+  0, scale_s,
   translation_x, translation_y,
 ].map {|x| "%f" % x } # Convert numbers to strings.
-regular_m_node["transform"] = "matrix(#{transform_matrix.join(" ")})"
+regular_s_node["transform"] = "matrix(#{transform_matrix.join(" ")})"
 
 # Replace the content of the #Regular-M node with the icon.
-regular_m_node.children = icon_svg.root.children.dup
+regular_s_node.children = icon_svg.root.children.dup
+
+# ultralight_s_node = symbol_svg.at_css("#Ultralight-S")
+# ultralight_s_node.children = icon_svg.root.children.dup
+
+# black_s_node = symbol_svg.at_css("#Black-S")
+# black_s_node.children = icon_svg.root.children.dup
 
 # Finish by writing the generated symbol to disk.
 File.open(DESTINATION_SVG_PATH, "w") do |f|
