7275493a8bef47c1781a0f87dc128ab792135cec
Tycho Tatitscheff <tychot@bam.tech>
2024-04-09 17:19:37 +0200

    test: add a test to generate the SVG

--- spec/sf_symbol_convertor_spec.rb
+++ spec/sf_symbol_convertor_spec.rb
@@ -4,4 +4,17 @@ require './lib/sf_symbol_converter'
 require 'nokogiri'
 
 describe SFSymbolConverter do
+  describe '#convert' do
+    it 'returns the converted SVG' do
+      template_svg = Nokogiri::XML(File.open('spec/fixtures/template.svg'))
+      icon_svg = Nokogiri::XML(File.open('spec/fixtures/icon.svg'))
+
+      converter = SFSymbolConverter.new(template_svg, icon_svg)
+      converted_svg = converter.convert
+
+      # puts Nokogiri::XML(converted_svg.to_s, &:noblanks).to_xml(indent: 2)
+
+      expect(converted_svg).to be_instance_of(Nokogiri::XML::Document)
+    end
+  end
 end
